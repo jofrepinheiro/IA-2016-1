c5b4ae5b357861024b0819a566c57169b8cf6ec3
jofrepinheiro <jofrepinheiro@gmail.com>
2016-09-26 00:27:55 -0300

    Test cases with 5 and 10 cities added.

--- 01 - Greedy/src/Main.java
+++ 01 - Greedy/src/Main.java
@@ -5,13 +5,14 @@ import greedy.Greedy;
 public class Main {
 	public static void main(String[] args) {
 		Greedy greedy = new Greedy();
-		String arq = "C:/Users/Jofre/Documents/GitHub/IA-2016-1/01 - Greedy/src/pr76.tsp";
+		String arq = "C:/Users/Jofre/Documents/GitHub/IA-2016-1/01 - Greedy/src/jofre10.tsp";
 		greedy.carregarTSP(arq); 
-		String opt = "C:/Users/Jofre/Documents/GitHub/IA-2016-1/01 - Greedy/src/pr76.opt.tour";
-		greedy.carregarOptTour(opt);
-		System.out.println(greedy.avaliar(greedy.opt_tour));	
+//		String opt = "C:/Users/Jofre/Documents/GitHub/IA-2016-1/01 - Greedy/src/pr76.opt.tour";
+//		greedy.carregarOptTour(opt);
+//		System.out.println(greedy.avaliar(greedy.opt_tour));	
+//		
+//		greedy.acharCaminho(greedy.distancias);
 		
-		greedy.acharCaminho(greedy.distancias);
-		
+		greedy.imprimirMatriz();
 	}
 }
